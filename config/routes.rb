365d5262169844e6f96ac12fb7c566102cb46fa1
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-08-08 16:48:17 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
       namespace :v1 do
           resources :users, only: [:create]
           resources :posts, only: [:create, :show, :index, :destroy]
+          resources :comments, only: [:create, :destroy]
           resources :likes, only: [:create, :destroy]
           post '/login', to: 'auth#create'
           get '/validate', to: 'auth#validate'
