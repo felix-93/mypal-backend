f6a0737525125655564c90d396f501880abc6ab1
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-08-13 13:41:38 +0100

    mvp

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -34,14 +34,44 @@ class Api::V1::PostsController < ApplicationController
         end
     end
 
+    def custom_index
+        index = scroll_params[:index]
+        if scroll_params[:filter] == "Trending"
+            @posts = Post.all.filter{|post| ((Time.now-post.created_at) / 3600) < 24 }
+            if @posts.length <= index
+                render json: { errors: "No Post left" }, status: :created
+            else
+                @posts = @posts.sort{|a, b| b.likes.length == a.likes.length ? b.created_at <=> a.created_at : b.likes.length <=> a.likes.length}[index..(index+3)]
+                render json: @posts
+            end
+        else
+            if Post.all.length <= index
+                render json: { errors: "No Post left" }, status: :created
+            else
+                @posts = Post.all.sort_by{|post| post.created_at}.reverse[index..(index+3)]
+                render json: @posts
+            end
+        end
+    end
+
     def myposts
         user = User.find(scroll_params[:user])
         index = scroll_params[:index]
-        if user.posts.length <= index+1
-            render json: { errors: "No Post left" }, status: :created
+        if scroll_params[:filter] == "Trending"
+            @posts = user.posts.filter{|post| ((Time.now-post.created_at) / 3600) < 24 }
+            if @posts.length <= index 
+                render json: { errors: "No Post left" }, status: :created
+            else
+                @posts = @posts.sort{|a, b| b.likes.length == a.likes.length ? b.created_at <=> a.created_at : b.likes.length <=> a.likes.length}[index..(index+3)]
+                render json: @posts
+            end
         else
-            @posts = user.posts.sort_by{|post| post.created_at}.reverse[index..(index+3)]
-            render json: @posts
+            if user.posts.length <= index 
+                render json: { errors: "No Post left" }, status: :created
+            else
+                @posts = user.posts.sort_by{|post| post.created_at}.reverse[index..(index+3)]
+                render json: @posts
+            end
         end
     end
 
@@ -53,7 +83,7 @@ class Api::V1::PostsController < ApplicationController
     end
 
     def scroll_params
-        params.permit(:index, :user)
+        params.permit(:index, :user, :filter)
     end
     
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :img, :username, :time_mark, :likes, :comments
+  attributes :id, :title, :content, :img, :username, :time_mark, :likes, :comments, :integerTime
 
   def username
     self.object.user.username
@@ -17,5 +17,8 @@ class PostSerializer < ActiveModel::Serializer
     self.object.comments.map{|comment| CommentSerializer.new(comment) }
   end
 
+  def integerTime
+    self.object.created_at.to_i
+  end
 
 end
